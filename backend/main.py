dd4afa760a5b3c0f1b245ba57e911932e03b1558
andrifernandiarif <andrifernandiarif@gmail.com>
2026-08-12 02:49:19 +0700

    Create initial KelanaAI console app

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
 # Now use them
 def print_trip_summary(
-    destination, 
-    days, 
-    budget, 
-    travel_style,
-    hotel_cost,
-    food_cost,
-    transportation_cost,
-    miscellaneous_cost
+    destination = input("Destination: "), 
+    country = input("Country: "),
+    days = int(input("Days: ")), 
+    budget = float(input("Budget: ")),
+    currency = input("Currency: "),
+    travel_style = input("Travel Style: "),
+    travel_month = input("Travel Month: "),
+    hotel_cost = float(input("Hotel Cost: ")),
+    food_cost = float(input("Food Cost: ")),
+    transportation_cost = float(input("Transportation Cost: ")),
+    miscellaneous_cost = float(input("Misc. Cost: "))
 ):
     total_estimated_cost = (
         hotel_cost
@@ -19,21 +22,24 @@ def print_trip_summary(
     print("======================")
     print("KelanaAI")
     print("======================")
-    print(f"Destination     : {destination}")
+    print(f"Destination     : {destination.capitalize()}")
+    print(f"Country         : {country.capitalize()}")
     print(f"Days            : {days}")
-    print(f"Budget          : {budget}")
-    print(f"Style           : {travel_style}")
-    print(f"Hotel Cost      : {hotel_cost}")
-    print(f"Food Cost       : {food_cost}")
-    print(f"Transport       : {transportation_cost}")
-    print(f"Misc Cost       : {miscellaneous_cost}")
-    print(f"Total           : {total_estimated_cost}")
+    print(f"Budget          : {budget} {currency.upper()}")
+    print(f"Currency        : {currency.upper()}")
+    print(f"Travel Month    : {currency.upper()}")
+    print(f"Style           : {travel_style.capitalize()}")
+    print(f"Hotel Cost      : {hotel_cost} {currency.upper()}")
+    print(f"Food Cost       : {food_cost} {currency.upper()}")
+    print(f"Transport       : {transportation_cost} {currency.upper()}")
+    print(f"Misc Cost       : {miscellaneous_cost} {currency.upper()}")
+    print(f"Total           : {total_estimated_cost} {currency.upper()}")
 
     if total_estimated_cost > budget:
-        print("⚠ Budget exceeded.")
-    
+        #print("⚠ Budget exceeded.")
+        print("\033[93m ⚠ Budget exceeded.\033[0m")
+
     print()
 
 # call it with any trip
-print_trip_summary("Japan", 5, 1500, "Family", 900, 300, 250, 100)
-print_trip_summary("Bali", 3, 800, "backpacker", 300, 150, 100, 75)
+print_trip_summary()
